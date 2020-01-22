e7a94d7a9d40ec5eb3c2a54d0845a6bfdde13571
Alexander Hoinville <ahoin001@gmail.com>
2020-01-21 22:37:29 -0500

    Create a server that accepts a post form and then returns the data, last taste of Node before using Express.js (Node.js framework)

--- Node Refresher/app.js
+++ Node Refresher/app.js
@@ -2,17 +2,47 @@ const http = require('http');
 
 // create server gets two arguments from server after making request 
 const server = http.createServer((req, res) => {
-    
+
     console.log(`INCOMING REQUEST`)
-    
+
     // Returns type of request method (Get, Post, Put etc)
-    console.log(`Request method: ${req.method}`); 
+    console.log(`Request method: ${req.method}`);
 
     // Returns URL requested
-    console.log(`Request URL: ${req.url}`);             
+    console.log(`Request URL: ${req.url}`);
 
-    // This is sent back to wherever request was made from
-    res.end('<h1>Success</h1>')
+    if (req.method === 'POST') {
+
+        let body = '';
+
+        // Starts once finished parsing data
+        req.on('end', () => {
+
+            // To see what is returned in req body ( name=alex )
+            console.log(`Req.body : ${body}`);
+
+            // get name from body
+            const userName = body.split('=')[1]
+
+            res.end(`<h1> ${userName}</h1>`)
+        })
+
+        // Starts when data is sent to data, callback automatically returns data of data recieved
+        req.on('data', (chunk) => {
+            body += chunk
+        })
+
+    } else {
+
+        // tells browser how to interpret our response
+        res.setHeader('Content-Type', 'text/html')
+
+        // This is sent back to wherever request was made from
+        res.end(`<form method="POST"> <input type=text name="username"> </input> <button>Create User</button> </form>`)
+
+    }
+
+
 });
 
 // listen is event listener for requests
